81eb6f353f9834a052cc005e31cb9bc82e26f64a
Muhammed Sinan <muhammedsinan7749@gmail.com>
2025-02-10 17:24:42 +0530

    delete task added

--- server/src/controllers/deleteTaskController.ts
+++ server/src/controllers/deleteTaskController.ts
@@ -1,11 +1,35 @@
 import { NextFunction, Request, Response } from "express";
+import { isValidObjectId } from "mongoose";
+import Task from "../model/Task";
 
 const deleteTaskController = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        
+        const taskId = req.params.id;
+
+        const validationError = await validate(taskId);
+        if (validationError) {
+            res.status(400).json({ message: validationError });
+            return;
+        }
+
+        await Task.findByIdAndDelete(taskId);
+        res.status(200).json({ message: "Task deleted successfully" });
     } catch (error) {
-        next(error)
+        next(error);
     }
 };
+
+const validate = async (taskId: string): Promise<string | null> => {
+    if (!isValidObjectId(taskId)) {
+       return "Invalid Id";
+    }
+
+    const task = await Task.findById(taskId);
+    if (!task) {
+        return "Task not found";
+    }
+    return null;
+};
+
 
 export default deleteTaskController;
